4b88765a0db99c0bd155b1c51dd597e31d71e1c2
AswathyMK <58411510+AswathyMK@users.noreply.github.com>
2020-01-03 11:57:31 +0530

    Program using Java Time.

---
+++ TimeExample1.java
@@ -0,0 +1,53 @@
+import java.time.LocalTime;
+public class TimeExample1 
+{
+
+	public static void main(String[] args) 
+	{
+		// current time
+		LocalTime localTime=LocalTime.now();
+		System.out.println(localTime);
+		
+		//specific time
+		LocalTime localTime2=LocalTime.of(4,30,45);
+		System.out.println(localTime2);
+		System.out.println("Hour : "+localTime.getHour());
+		System.out.println("Minute :"+localTime.getMinute());
+		System.out.println("Second :"+localTime.getSecond());
+		
+		//localTime plus methods
+		System.out.println("Addition of hours :"+localTime.plusHours(2));
+		System.out.println("Addition of minutes :"+localTime.plusMinutes(25));
+		System.out.println("Addition of seconds :"+localTime.plusSeconds(30));
+		System.out.println("Addition of Nanos :"+localTime.plusNanos(60000));
+		
+		//localTime minus methods
+		System.out.println("subtraction of hours :"+localTime.minusHours(2));
+		System.out.println("subtraction of minutes :"+localTime.minusMinutes(25));
+		System.out.println("subtraction of seconds :"+localTime.minusSeconds(30));
+		System.out.println("subtraction of Nanos :"+localTime.minusNanos(60000));
+
+        LocalTime localTime3=LocalTime.of(11, 45, 20);
+        //compare to example
+        if(localTime.compareTo(localTime2)==0)
+        {
+        	System.out.println("localDate1 and localDate2 are equal");
+        }else
+        {
+    	    System.out.println("localDate1 and localDate2 are not equal");
+        }
+       
+        //isBefore() example
+        if(localTime2.isBefore(localTime3))
+        {
+    	    System.out.println("localDate2 comes  before localTime3");
+        }
+        //is after example
+        if(localTime3.isBefore(localTime))
+        {
+    	    System.out.println("localDate3 comes after localTime1");
+        }
+
+	}
+
+}
