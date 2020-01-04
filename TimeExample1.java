10d640b5d1cc3a6dde4e029eed53fa23d9aa7e70
AswathyMK <58411510+AswathyMK@users.noreply.github.com>
2020-01-04 10:20:49 +0530

    Program using Java DateTime

--- TimeExample1.java
+++ TimeExample1.java
@@ -27,27 +27,25 @@ public class TimeExample1
 		System.out.println("subtraction of seconds :"+localTime.minusSeconds(30));
 		System.out.println("subtraction of Nanos :"+localTime.minusNanos(60000));
 
-        LocalTime localTime3=LocalTime.of(11, 45, 20);
-        //compare to example
-        if(localTime.compareTo(localTime2)==0)
-        {
-        	System.out.println("localTime1 and localTime2 are equal");
-        }else
-        {
-    	    System.out.println("localTime1 and localTime2 are not equal");
-        }
+                LocalTime localTime3=LocalTime.of(11, 45, 20);
+       	         //compare to example
+        	if(localTime.compareTo(localTime2)==0)
+       	        {
+        		System.out.println("localTime1 and localTime2 are equal");
+        	}else
+        	{
+    	   	 	System.out.println("localTime1 and localTime2 are not equal");
+        	}
        
-        //isBefore() example
-        if(localTime2.isBefore(localTime3))
-        {
-    	    System.out.println("localTime2 comes  before localTime3");
-        }
-        //is after example
-        if(localTime3.isBefore(localTime))
-        {
-    	    System.out.println("localTime3 comes after localTime1");
-        }
-
+       	        //isBefore() example
+        	if(localTime2.isBefore(localTime3))
+        	{
+    	    	       System.out.println("localTime2 comes  before localTime3");
+        	}
+        	//is after example
+        	if(localTime3.isBefore(localTime))
+       	        {
+    	       	      System.out.println("localTime3 comes after localTime1");
+                }
 	}
-
 }
